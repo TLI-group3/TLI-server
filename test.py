f3001f26adb996f2d4016258d6e4dad78fcd7735
dhruvaa02 <dhruvaa.saravanan@gmail.com>
2021-10-21 00:22:55 -0400

    cors?

--- test.py
+++ test.py
@@ -11,3 +11,11 @@ async def root():
 @app.get("/demo")
 async def root():
     return {"cars": [{"Lamborghini": {'Aventador', 'S', '2020', 100000}}, {"Ford": {'Explorer', 'SUV', '2021', 50000}}]}
+
+
+@app.after_request
+def after_request(response):
+  response.headers.set('Access-Control-Allow-Origin', '*')
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response 
