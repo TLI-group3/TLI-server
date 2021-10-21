d16ae702950ccce4c11a913aaf892114df09532c
dhruvaa02 <dhruvaa.saravanan@gmail.com>
2021-10-21 00:17:26 -0400

    cars list

--- src/apiCallTest.java
+++ src/apiCallTest.java
@@ -1,31 +1,24 @@
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-
-public class apiCallTest {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        executePost();
-    }
-
-    public static void executePost() throws IOException, InterruptedException {
-        // create a client
-        HttpClient client = HttpClient.newHttpClient();
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
  
-        // create a request
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://18.222.134.223/demo"))
-                .header("Content-Type", "application/json")
-                .GET(HttpRequest.BodyPublishers.ofString())
-                .build();
-
-        // use the client to send the request
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        // the response:
-        System.out.println(response.body());
+class Main
+{
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://reqres.in/api/users?page=1");
+        URLConnection connection = url.openConnection();
+ 
+        // `HttpURLConnection` connection = (HttpURLConnection) url.openConnection();
+        // connection.setRequestMethod("GET");
+ 
+        try (BufferedReader in = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream())))
+        {
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
     }
 }
