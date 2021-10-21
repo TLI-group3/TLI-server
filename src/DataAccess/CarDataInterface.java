f40ad145a1ca7b73fea3f865406f3dc955e46790
Leen Al Lababidi <leenlab2@gmail.com>
2021-10-20 22:26:57 -0400

    Accessing car data

--- src/DataAccess/CarDataInterface.java
+++ src/DataAccess/CarDataInterface.java
@@ -1,4 +1,10 @@
 package DataAccess;
 
+import Entities.Car;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public interface CarDataInterface {
+    public ArrayList<Car> loadCarData(String file) throws IOException, ClassNotFoundException, InterruptedException;
 }
