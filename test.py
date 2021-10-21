762b76d887d7b0a584363f057639a8c6fa281bf0
dhruvaa02 <dhruvaa.saravanan@gmail.com>
2021-10-20 23:41:13 -0400

    cars list

--- test.py
+++ test.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/demo")
+async def root():
+    return {"cars": [{"Lamborghini": {'Aventador', 'S', '2020', 100000}}, {"Ford": {'Explorer', 'SUV', '2021', 50000}}]}
