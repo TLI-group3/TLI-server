28c350560106483b97c4fabf15cdbce945c689df
Leen Al Lababidi <leenlab2@gmail.com>
2021-10-22 11:58:02 -0400

    Changed to POST request

--- src/main/java/com/aviva/APIBoundary/HelloController.java
+++ src/main/java/com/aviva/APIBoundary/HelloController.java
@@ -6,24 +6,19 @@ import main.java.com.aviva.DataAccess.CSVAccountHolderData;
 import main.java.com.aviva.Entities.AccountHolder;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
 public class HelloController {
-
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-
     @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/cars")
-    public String getCars() throws IOException, InterruptedException, ClassNotFoundException {
+    @PostMapping("/cars")
+    public String getCars(@RequestBody String clientID) throws IOException, InterruptedException, ClassNotFoundException {
         AccountHolderDataInterface accountData = new CSVAccountHolderData();
-        AccountHolder user = accountData.getClientByID("5002357538983918");
+        AccountHolder user = accountData.getClientByID(clientID);
         JSONObject cars = CarRecommender.getRecommendedCars(user);
         System.out.println(cars);
         return cars.toString();
