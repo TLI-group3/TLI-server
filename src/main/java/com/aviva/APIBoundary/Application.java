342cd90daab3e683b3b1575949f0b29d5159d418
Leen Al Lababidi <leenlab2@gmail.com>
2021-10-22 17:54:49 -0400

    fixed config imports

--- src/main/java/com/aviva/APIBoundary/Application.java
+++ src/main/java/com/aviva/APIBoundary/Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
