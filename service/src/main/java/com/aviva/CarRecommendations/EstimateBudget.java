97c9fe683c8b63e8980fdf56fb29e6082bb48f30
An Nguyen-Trinh <an.nguyentrinh@mail.utoronto.ca>
2021-11-03 22:28:36 -0400

    Putting down the draft of what the budget calculator will do

--- service/src/main/java/com/aviva/CarRecommendations/EstimateBudget.java
+++ service/src/main/java/com/aviva/CarRecommendations/EstimateBudget.java
@@ -1,4 +1,10 @@
 package main.java.com.aviva.CarRecommendations;
 
 public class EstimateBudget {
+    float totalWithdraws = 0;
+    float totalDeposits = 0;
+
+    float annualBudget = totalDeposits - totalWithdraws;
+
+    float monthlyBudget = annualBudget / 12;
 }
