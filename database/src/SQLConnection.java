8bcb32fb7d2d75968f88e73c8cb55379c7b65a91
Alamgir Khan <55143972+Alamgir-K@users.noreply.github.com>
2021-11-07 21:02:23 -0500

    Adding documentation to SQLConnection file

--- database/src/SQLConnection.java
+++ database/src/SQLConnection.java
@@ -1,30 +1,33 @@
 /*
- * This is a temporary file to see if we can insert data into the database
+ * Establish connection with RDS instance
  */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class SQLConnection {
-
+    
+    // RDS Credentials
     public static String url = "jdbc:mysql://cb-database.c0bn4vphjqef.us-east-2.rds.amazonaws.com";
     public static String user = "Alamgir";
     public static String password = "tligroup3";
 
     public static void main(String[] args) {
+        // Attempt to connect to RDS instance
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement();
-
             System.out.println("Successfully connected");
         }
+        // Print error statement if connection fails
         catch (SQLException e) {
             System.out.println(e);
         }
     }
-
+    
+    /**
+    * Inserts a string into a dummy table in the database
+    */
     public static void insertInstallments(Connection connection, String intallments) {
         try {
             Statement statement = connection.createStatement();
