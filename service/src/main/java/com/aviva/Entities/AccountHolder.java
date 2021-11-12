321e40469048481deed5ccdf13e604eafb3ecb0b
An Nguyen-Trinh <an.nguyentrinh@mail.utoronto.ca>
2021-11-12 17:15:12 -0500

    I removed the no longer necessary setter or getters methods for the Account Holder entity. Additionally, I also added new private instance variables to keep the information about the account holder, and I also added new setter and getters methods for those. Lastly, I documented all methods in the file.

--- service/src/main/java/com/aviva/Entities/AccountHolder.java
+++ service/src/main/java/com/aviva/Entities/AccountHolder.java
@@ -3,34 +3,102 @@ import org.json.JSONObject;
 
 
 /**
- * This class represents an account holder at the bank
+ * This class represents an account holder at a given bank.
  */
 public class AccountHolder {
+    // General financial Info
     private String accountNumber;
     private int creditScore;
-    private float monthlyBudget;
+    private float monthlySalary;
 
-    // A temporarily private instance to store all the financial transactions
-    private JSONObject financialTransactions;
+    // Information relating to lifestyle
+    private float monthlyBudget;
+    private float existingCarLoan;
+    private String existingCar;
+    private float otherMonthlySpending;
 
     public AccountHolder(String accountNumber){
         this.accountNumber = accountNumber;
     }
 
-    public void setCreditScore(int creditScore) {this.creditScore = creditScore;}
-
-    public void setMonthlyBudget(float monthlyBudget) {this.monthlyBudget = monthlyBudget;}
-
+    /**
+     * Returns the account number of this account holder.
+     * @return string representing account number.
+     */
     public String getAccountNumber() {return this.accountNumber;}
 
+    /**
+     * Sets the most recent credit score of this account holder.
+     * @param credit score
+     */
+    public void setCreditScore(int creditScore) {this.creditScore = creditScore;}
+
+    /**
+     * Sets the monthly salary of this account holder.
+     * @param monthly salary
+     */
+    public void setMonthlySalary(int monthlySalary) {this.monthlySalary = monthlySalary;}
+
+    /**
+     * Set the calculated estimated budget of this account holder.
+     * @param estimated budget of this account holder
+     */
+    public void setMonthlyBudget(float monthlyBudget) {this.monthlyBudget = monthlyBudget;}
+
+    /**
+     * Sets the existing car loan of this account holder.
+     * @param car loan
+     */
+    public void setExistingCarLoan(float loan) {this.existingCarLoan = loan;}
+
+    /**
+     * Sets the existing vehicle of this account holder.
+     * @param car name
+     */
+    public void setExistingCar(String ownedCar) {this.existingCar = ownedCar;}
+
+    /**
+     * Sets the total of the monthly spending of this account holder.
+     * @param monthly spending
+     */
+    public void setOtherMonthlySpending(float monthlySpending) {this.otherMonthlySpending = monthlySpending;}
+
+    /**
+     * Returns the most recent credit score this account holder.
+     * @return credit score.
+     */
     public int getCreditScore() {return this.creditScore;}
+
+
+    /**
+     * Returns the monthly salary of this account holder.
+     * @return monthly salary
+     */
+    public void getMonthlySalary() {return this.monthlySalary;}
+
+    /**
+     * Returns calculated estimated budget of this account holder.
+     * @return credit score.
+     */
 
     public float getMonthlyBudget() {return this.monthlyBudget;}
 
-    // A temporarily setter method to store all the financial transactions
-    public void setFinancialTransactions(JSONObject financialTransactions) {
-        this.financialTransactions=financialTransactions;}
+    /**
+     * Returns the existing car loan of this account holder.
+     * @return car loan
+     */
+    public void getExistingCarLoan() {return this.existingCarLoan;}
 
-    // A temporarily getter method to return all the financial transactions
-    public JSONObject getFinancialTransactions() {return this.financialTransactions;}
+    /**
+     * Returns the existing vehicle of this account holder.
+     * @return car name
+     */
+    public void getExistingCar() {return this.existingCar;}
+
+    /**
+     * Returns the total of the monthly spending of this account holder.
+     * @return monthly spending
+     */
+    public void setOtherMonthlySpending() {return this.otherMonthlySpending;}
+
 }
