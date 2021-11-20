a39ad10ad0cfb54f3b571e8cb378bcacf7539816
An Nguyen-Trinh <an.nguyentrinh@mail.utoronto.ca>
2021-11-20 16:13:41 -0500

    Created the method to identify the bi-monthly salary

--- src/main/java/com/aviva/CarRecommendations/EstimateBudget.java
+++ src/main/java/com/aviva/CarRecommendations/EstimateBudget.java
@@ -1,14 +1,19 @@
 package com.aviva.CarRecommendations;
 
 import com.aviva.DataAccess.BankingDataProcess;
+import com.aviva.DataAccess.BankingDataProcessingInterface;
 import com.aviva.Entities.AccountHolder;
-import java.util.ArrayList;
+
+import java.util.*;
 
 /**
  * Public class that handles the business logic of estimating the budget of a particular AccountHolder
  */
 
 public class EstimateBudget {
+    static float MINIMUM_BIMONTHLY_SALARY = 1000;
+    static int SALARY_FREQUENCY = 20;
+    BankingDataProcessingInterface bdpInit = new BankingDataProcess();
 
     /**
      * Return the yearly budget of an AccountHolder as a float
@@ -16,35 +21,34 @@ public class EstimateBudget {
      * @param user the AccountHolder for which to generate the budget
      * @return float representing the AccountHolder's yearly budget.
      */
-    public float calculateYearlyBudget(AccountHolder user) {
-        // Get AccountHolder's deposits
-        BankingDataProcess bdpInit = new BankingDataProcess();
-        ArrayList<Float> deposits = bdpInit.getDeposits(user.getAccountNumber());
+    float identifyMonthlySalary(AccountHolder account) {
 
-        // Sum all deposits
-        float yearlyIn = 0;
+        ArrayList<Float> deposits = bdpInit.getDeposits(account.getAccountNumber());
+
+        // Mapping the possible deposits that could be the bimonthly salary to its number of occurrence
+        Map frequency = new HashMap();
+
+        // Identify the possible deposits that could be the bimonthly salary
         for (float i : deposits) {
-            yearlyIn += i;
+            if (i >= MINIMUM_BIMONTHLY_SALARY) {
+                if (frequency.containsKey(i)) {
+                    int curr = (int) frequency.get(i);
+                    frequency.put(i, curr + 1);
+                } else {
+                    frequency.put(i, 1);
+                }
+            }
         }
 
-        // Get all withdrawals and sum them
-        ArrayList<Float> withdrawals = bdpInit.getWithdrawals(user.getAccountNumber());
-        float yearlyOut = 0;
-        for (float i : withdrawals) {
-            yearlyOut += i;
+        float possibleSalary = 0;
+
+        // Identify the possible salary
+        for (Object curr : frequency.keySet()) {
+            int currValue = (int) frequency.get(curr);
+            if (currValue >= SALARY_FREQUENCY && ((float) curr) == possibleSalary) {
+                possibleSalary = (float) curr;
+            }
         }
-
-        return yearlyIn - yearlyOut;
+        return possibleSalary;
     }
-
-    /**
-     * Return the monthly budget of an AccountHolder as a float
-     *
-     * @param user the AccountHolder for which to generate the budget
-     * @return float representing the AccountHolder's monthly budget.
-     */
-    public float calculateMonthlyBudget(AccountHolder user) {
-        return calculateYearlyBudget(user) / 12;
-    }
-
 }
