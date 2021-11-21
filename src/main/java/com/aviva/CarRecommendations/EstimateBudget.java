1775a0e80e16152e18023d251ff0f895a0e38890
An Nguyen-Trinh <an.nguyentrinh@mail.utoronto.ca>
2021-11-21 16:35:48 -0500

    Created the method that identify the major monthly spendings and the method setting the monthly budget

--- src/main/java/com/aviva/CarRecommendations/EstimateBudget.java
+++ src/main/java/com/aviva/CarRecommendations/EstimateBudget.java
@@ -12,25 +12,94 @@ import java.util.*;
 
 public class EstimateBudget {
     static float MINIMUM_BIMONTHLY_SALARY = 1000;
-    static int SALARY_FREQUENCY = 20;
+    static int BIMONTHLY_SALARY_FREQUENCY = 24;
+    static int MONTHLY_SPENDING_FREQUENCY = 12;
+    static float MINIMUM_MONTHLY_CAR_LOAN = 50;
     BankingDataProcessingInterface bdpInit = new BankingDataProcess();
 
     /**
-     * Return the yearly budget of an AccountHolder as a float
+     * Set the monthly salary of an AccountHolder based on studying the deposits of this AccountHolder
      *
-     * @param user the AccountHolder for which to generate the budget
-     * @return float representing the AccountHolder's yearly budget.
+     * @param account the AccountHolder for which to generate the budget
      */
-    float identifyMonthlySalary(AccountHolder account) {
+    void identifyBiMonthlySalary(AccountHolder account) {
 
         ArrayList<Float> deposits = bdpInit.getDeposits(account.getAccountNumber());
 
         // Mapping the possible deposits that could be the bimonthly salary to its number of occurrence
         Map frequency = new HashMap();
 
+        extractMajorCashFlow(deposits, frequency, MINIMUM_BIMONTHLY_SALARY);
+
+        float possibleSalary = 0;
+        int possibleSalaryFrequency = 0;
+
+        // Identify the possible salary
+        for (Object curr : frequency.keySet()) {
+            int currFrequency = (int) frequency.get(curr);
+            if (currFrequency >= BIMONTHLY_SALARY_FREQUENCY && ((float) curr) == possibleSalary) {
+                possibleSalary = (float) curr;
+                possibleSalaryFrequency = currFrequency;
+            }
+        }
+
+
+       account.setMonthlySalary(possibleSalary * (possibleSalaryFrequency / 12));
+    }
+
+    /**
+     * Set the other monthly spending, and optionally an existing car loan,
+     * of an AccountHolder based on studying the deposits of this AccountHolder
+     *
+     * @param account the AccountHolder for which to generate the budget
+     */
+    void identifySpending(AccountHolder account) {
+        ArrayList<Float> withdrawals = bdpInit.getWithdrawals(account.getAccountNumber());
+
+        // Mapping the possible deposits that could be the bimonthly salary to its number of occurrence
+        Map frequency = new HashMap();
+
         // Identify the possible deposits that could be the bimonthly salary
-        for (float i : deposits) {
-            if (i >= MINIMUM_BIMONTHLY_SALARY) {
+        extractMajorCashFlow(withdrawals, frequency, MINIMUM_MONTHLY_CAR_LOAN);
+
+        Set monthlySpendings = new HashSet();
+
+        // Identify the monthly occurring withdrawals
+        for (Object curr : frequency.keySet()) {
+            int currFrequency = (int) frequency.get(curr);
+            if (currFrequency >= MONTHLY_SPENDING_FREQUENCY) {
+               monthlySpendings.add(curr);
+            }
+        }
+
+        // Assuming the biggest monthly withdrawal is for rent, so it is set to the other monthly spending
+        account.setOtherMonthlySpending((float) Collections.max(monthlySpendings));
+        monthlySpendings.remove(Collections.max(monthlySpendings));
+
+        // If there is more than 1 monthly withdrawal, assume the next largest spending is for the existing car loan
+        if (!monthlySpendings.isEmpty()) {
+            account.setExistingCarLoan((float) Collections.max(monthlySpendings));
+        }
+    }
+
+    /**
+     * Set the monthly budget of an AccountHolder assuming that at least their monthly salary and other montly
+     * spending are known.
+     *
+     * @param account the AccountHolder for which to generate the budget
+     */
+    void determineMonthlyBudget(AccountHolder account) {
+        if (account.getExistingCarLoan() == 0) {
+            float monthlyBudget = (float) ((account.getMonthlySalary() - account.getOtherMonthlySpending()) * 0.1;
+            account.setMonthlyBudget(monthlyBudget);
+        } else {
+            account.setMonthlyBudget(account.getExistingCarLoan());
+        }
+    }
+
+    private void extractMajorCashFlow(ArrayList<Float> cashflow, Map frequency, float lowerBound) {
+        for (float i : cashflow) {
+            if (i >= lowerBound) {
                 if (frequency.containsKey(i)) {
                     int curr = (int) frequency.get(i);
                     frequency.put(i, curr + 1);
@@ -39,16 +108,5 @@ public class EstimateBudget {
                 }
             }
         }
-
-        float possibleSalary = 0;
-
-        // Identify the possible salary
-        for (Object curr : frequency.keySet()) {
-            int currValue = (int) frequency.get(curr);
-            if (currValue >= SALARY_FREQUENCY && ((float) curr) == possibleSalary) {
-                possibleSalary = (float) curr;
-            }
-        }
-        return possibleSalary;
     }
 }
