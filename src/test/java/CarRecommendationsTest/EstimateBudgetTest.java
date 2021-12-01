ad0f2abbd3a2bc9e408e92203b5f31165deb8e5f
An Nguyen-Trinh <an.nguyentrinh@mail.utoronto.ca>
2021-12-01 10:23:46 -0500

    Added testing for EstimateBudget class

--- src/test/java/CarRecommendationsTest/EstimateBudgetTest.java
+++ src/test/java/CarRecommendationsTest/EstimateBudgetTest.java
@@ -1,29 +1,30 @@
-//package CarRecommendationsTest;
-//
-//import com.aviva.CarRecommendations.EstimateBudget;
-//import com.aviva.Entities.AccountHolder;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public class EstimateBudgetTest {
-//    EstimateBudget classToTest;
-//    AccountHolder knownAccount;
-//
-//    @BeforeEach
-//    public void setup() {
-//        classToTest = new EstimateBudget();
-//        knownAccount = new AccountHolder("1402110922112412");
-//    }
-//
-//    @Test
-//    public void testCalculateYearlyBudget() {
-//        assertEquals(38460.0F, classToTest.calculateYearlyBudget(knownAccount));
-//    }
-//
-//    @Test
-//    public void testCalculateMonthlyBudget() {
-//        assertEquals(3205.0F, classToTest.calculateMonthlyBudget(knownAccount));
-//    }
-//}
+package CarRecommendationsTest;
+
+import com.aviva.CarRecommendations.EstimateBudget;
+import com.aviva.Entities.AccountHolder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EstimateBudgetTest {
+    EstimateBudget estimatorToTest;
+    AccountHolder knownAccount;
+
+    @BeforeEach
+    public void setup() {
+        knownAccount = new AccountHolder("1402110922112412");
+        estimatorToTest = new EstimateBudget(1, knownAccount);
+        estimatorToTest.performTask();
+    }
+
+    @Test
+    public void testEstimateBudget() {
+        assertEquals(5500, knownAccount.getMonthlySalary());
+        assertEquals(1995, knownAccount.getOtherMonthlySpending());
+        assertEquals(800, knownAccount.getExistingCarLoan());
+        assertEquals(800, knownAccount.getMonthlyBudget());
+        assertEquals(719, knownAccount.getCreditScore());
+    }
+}
+
