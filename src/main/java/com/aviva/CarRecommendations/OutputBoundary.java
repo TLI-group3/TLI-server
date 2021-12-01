e1ebe86f55ffb7fa9186bd14891d194261c87132
dhruvaa02 <86129180+dhruvaa02@users.noreply.github.com>
2021-12-01 17:24:43 -0500

    Gave example output string for output

--- src/main/java/com/aviva/CarRecommendations/OutputBoundary.java
+++ src/main/java/com/aviva/CarRecommendations/OutputBoundary.java
@@ -19,7 +19,10 @@ public class OutputBoundary {
     /**
      * Converst a hasmap of car, loan to a json formatted string
      * @param recommended the filtered recommended cars to convert
-     * @return a JSON formatted string containing all information of cars and their loans
+     * @return a JSON formatted string containing all information of cars and their loans e.g.:
+     * {"cars":[{"image":"https://",
+     *           "loan":{"interestRate":2.69,"capitalSum":6600,"loanTerm":36,"loanAmount":6600,"interestSum":277.29,"loanSum":6877.29},
+     *           "year":2018,"price":6000,"model":"malibu","make":"chevrolet"}
      */
     public String convert(HashMap<Car, Loan> recommended){
         JSONArray carInfo = new JSONArray();
